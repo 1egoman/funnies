aea672af2315ea0cff7ca4450c2967606e15f0ae
Keari Eggers <kungfu.keari@gmail.com>
2016-10-16 15:03:41 -0500

    add separate gulp task for production

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,13 @@ import browserify from 'browserify';
 gulp.task('default', () => {
   browserify('./src/index.js')
     .transform('babelify', {presets: ['es2015', 'react']})
+    .bundle()
+    .pipe(fs.createWriteStream('./dist/bundle.js'));
+});
+
+gulp.task('production', () => {
+  browserify('./src/index.js')
+    .transform('babelify', {presets: ['es2015', 'react']})
     .transform('uglifyify', {global: true})
     .bundle()
     .pipe(fs.createWriteStream('./dist/bundle.js'));
