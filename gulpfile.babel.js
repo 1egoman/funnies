623d0769a251daeeb5e930e86214a54790f991b8
Keari Eggers <kungfu.keari@gmail.com>
2016-10-16 13:42:10 -0500

    add simple gulpfile
    Includes babel transpiling, uglifying, and bundling of JS.

---
+++ gulpfile.babel.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import gulp from 'gulp';
+import browserify from 'browserify';
+
+gulp.task('default', () => {
+  browserify('./src/index.js')
+    .transform('babelify', {presets: ['es2015', 'react']})
+    .transform('uglifyify', {global: true})
+    .bundle()
+    .pipe(fs.createWriteStream('./dist/bundle.js'));
+});
