08b2751eb73263da9f16c3e6c2e54b8f797cf970
Raphael Brasil <raphael.brasil93@gmail.com>
2016-10-18 19:30:04 -0200

    Adds more funny phrases (#42)

--- src/funnies.js
+++ src/funnies.js
@@ -122,4 +122,6 @@ export default [
   "If I’m not back in five minutes, just wait longer.",
   "Some days, you just can’t get rid of a bug!",
   "We’re going to need a bigger boat.",
+  "Chuck Norris never git push. The repo pulls before.",
+  "Web developers do it with <style>"
 ];
