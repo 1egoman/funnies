9fd4951dda47b1635f7962b170b90d98ddef8d8d
Keari Eggers <kungfu.keari@gmail.com>
2016-10-18 10:30:29 -0500

    change default task
    Running the default gulp task now does the following:
    
    - Transpiles the source code using Babel and drops it into the dist
    directory
    so that require works (gulp babel)
    - Creates an unmified dev version of the bundle  (gulp dev)
    - Creates a minified, production version of the bundle (gulp prod)
    
    Development version of bundle is called funnies.dev.js so that it does
    not conflict with the funnies data.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import path from 'path';
 import gulp from 'gulp';
 import browserify from 'browserify';
 import babel from 'gulp-babel';
+import source from 'vinyl-source-stream';
 
 const paths = {
   src: './src',
@@ -10,25 +11,27 @@ const paths = {
 };
 
 const input = path.join(paths.src, 'index.js');
-const output = {
-  dev: path.join(paths.dest, 'funnies.js'),
-  prod: path.join(paths.dest, 'funnies.min.js'),
+const outputName = {
+  dev: 'funnies.dev.js',
+  prod: 'funnies.min.js',
 };
 
-gulp.task('default', () => {
+gulp.task('dev', () => {
   browserify(input)
     .transform('babelify', {presets: ['es2015', 'react']})
     .bundle()
-    .pipe(fs.createWriteStream(output.dev));
+    .pipe(source(outputName.dev))
+    .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('production', () => {
+gulp.task('prod', () => {
   browserify(input)
     .transform('envify', {global: true, _: 'purge', NODE_ENV: 'production'})
     .transform('babelify', {presets: ['es2015', 'react']})
     .transform('uglifyify', {global: true})
     .bundle()
-    .pipe(fs.createWriteStream(output.prod));
+    .pipe(source(outputName.prod))
+    .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('babel', () => {
@@ -36,3 +39,5 @@ gulp.task('babel', () => {
     .pipe(babel())
     .pipe(gulp.dest(paths.dest))
 });
+
+gulp.task('default', ['babel', 'dev', 'prod']);
