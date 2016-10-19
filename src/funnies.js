b34969fed852081a293dfe3fec7e78c5143ec00e
Rusty Bailey <rustylbailey@gmail.com>
2016-10-19 17:29:40 -0400

    Add 'Dividing by zero' as a funny. (#43)

--- src/funnies.js
+++ src/funnies.js
@@ -119,6 +119,7 @@ export default [
   "Everything sounds the same",
   "I'm going to walk the dog",
   "I didn't choose the engineering life. The engineering life chose me.",
+  "Dividing by zero...",
   "Spawn more Overlord!",
   "If I’m not back in five minutes, just wait longer.",
   "Some days, you just can’t get rid of a bug!",
