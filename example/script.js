ad8d2bd3eb45e14fe12ae01dfb2f93b4014dc7d1
Ryan Gaus <rsg1egoman@gmail.com>
2016-12-12 21:02:41 -0500

    code: add an example of both the component and the class

--- example/script.js
+++ example/script.js
@@ -1,5 +1,14 @@
+import {
+  FunniesComponent as Component,
+  Funnies,
+} from '../src';
+
+// Funnies class
+let funnies = new Funnies();
+document.getElementById('funnies').innerHTML = funnies.message();
+
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {FunniesComponent as Component} from '../src';
 
-ReactDOM.render(<Component />, document.getElementById('root'));
+ReactDOM.render(<Component />, document.getElementById('react'));
