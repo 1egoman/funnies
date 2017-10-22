f593a3d7aea346e00cfb08de826d3e32ab9700de
farque65 <fahim.farque@gmail.com>
2017-10-22 16:21:09 -0400

    added more funnies

--- src/funnies.js
+++ src/funnies.js
@@ -71,5 +71,7 @@ export default [
   "Putting the icing on the cake. The cake is not a lie...",
   "Cleaning off the cobwebs...",
   "Making sure all the i's have dots...",
-  "We are not liable for any broken screens as a result of waiting."
+  "We are not liable for any broken screens as a result of waiting.",
+  "We need more dilithium crystals",
+  "Where did all th internets go"
 ];
