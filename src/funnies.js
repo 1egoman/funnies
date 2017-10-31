302a8711736c5dba61cf0ea1bbe03f62b5ecca29
Alison Jo McCauley <alisonjo2786@users.noreply.github.com>
2017-10-31 18:54:00 -0400

    Typo line 101

--- src/funnies.js
+++ src/funnies.js
@@ -98,7 +98,7 @@ export default [
   "How did you get here?",
   "Wait, do you smell something burning?",
   "Computing the secret to life, the universe, and everything.",
-  "When nothing is going right, Go left!!...",
+  "When nothing is going right, go left!!...",
   "I love my job only when I'm on vacation...",
   "i'm not lazy, I'm just relaxed!!",
   "Never steal. The government hates competition....",
