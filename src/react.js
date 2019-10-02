803b5f51b92a587534c88e180d811305ca4fc18f
Ryan Gaus <rsg1egoman@gmail.com>
2019-10-01 21:58:07 -0400

    Update package versions (#111)
    * fix: add node v12 to ci tests, update version of react
    
    * dist: update built docs bundle
    
    * fix: update gulp to v4 so that it works on node v12

--- src/react.js
+++ src/react.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Funnies from './index';
 
 export default class FunniesComponent extends React.Component {
