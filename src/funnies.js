0afb1a594be5ce542bcb4b36d319eb35bcb35e5f
Teodor <PixellUp@users.noreply.github.com>
2019-10-15 18:54:04 +0300

    few jokes (#113)

--- src/funnies.js
+++ src/funnies.js
@@ -258,5 +258,7 @@ export default [
   "Loading Error...",
   "PC LOAD LETTER...",
   "Waiting for something to load sucks...",
-  "Deleting system32..."
+  "Deleting system32...",
+  "- What are you working? - I'm a programmer. - More precisely? - At home, I change TV programs.",
+  "New LG Refrigerators with Windows 10 ... 'A cold brandy pleats!' - Server Error 404, no Brandy available."
 ];
